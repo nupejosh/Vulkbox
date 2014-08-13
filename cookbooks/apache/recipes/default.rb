3bd2d54e5c3e5d35b4fe398e82ec2eeacec30494
Jordan Johnson <JordanJohnson@jordans-air.attlocal.net>
2014-08-12 19:06:00 -0500

    Added apache recipe and homepage for cookbook file

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,23 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+	
+package 'apache2' do
+	action :install 
+end 
+
+service 'apache2' do 
+	action [ :enable, :start ] 
+end 
+
+cookbook_file '/var/www/index.html' do 
+	source 'index.htnl' 
+	mode '0644' 
+end 
+
+0
